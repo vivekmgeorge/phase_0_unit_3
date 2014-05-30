8e2fc38b16e7bea43ac2f627a8a1b0319f2fb29a
Vivek M George <vivek.m.george@gmail.com>
2014-05-30 18:49:37 -0400

    completed hw

--- week_8_and_9/3_Ruby/fibonacci_sequence/my_solution.rb
+++ week_8_and_9/3_Ruby/fibonacci_sequence/my_solution.rb
@@ -8,10 +8,22 @@
 
 
 # 3. Initial Solution
+require "bigdecimal"
 
 def is_fibonacci?(num)
-
+ num = BigDecimal.new(num)
+ bigNumber1 = (5*(num**2)+4).sqrt(0)
+ bigNumber2 = (5*(num**2)-4).sqrt(0)
+ return (bigNumber1 == bigNumber1.round || bigNumber2 == bigNumber2.round)
 end
+
+# is_fibonacci? 927372692193078999171 # => false
+# is_fibonacci? 927372692193078999176 # => true
+
+# def is_fibonacci?(num)
+# 	if n == 0
+
+# end
 
 
 
