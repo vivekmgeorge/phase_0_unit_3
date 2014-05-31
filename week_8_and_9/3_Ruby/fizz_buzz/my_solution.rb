11d0b58a43f45a2074f295ba2fc6c4215d3e90d0
Vivek M George <vivek.m.george@gmail.com>
2014-05-30 20:44:39 -0400

    completed hw

--- week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
+++ week_8_and_9/3_Ruby/fizz_buzz/my_solution.rb
@@ -8,13 +8,44 @@
 
 
 # 3. Initial Solution
-
 def super_fizzbuzz(array)
+	answers = []
+	array.each do |num|
+		if num % 3 == 0 and num % 5 == 0 
+			num = "FizzBuzz"
+		elsif num % 3 == 0
+			num = "Fizz"
+		elsif num % 5 == 0 
+			num = "Buzz"
+		else
+			num
+		end
+		answers << num
+	end
+	answers
 
 
+# 	return 'FizzBuzz' if is_divisible_by_fifteen?(array) 
+# 	return 'Buzz' if is_divisible_by_five?(array)
+# 	return 'Fizz' if is_divisible_by_three?(array)
+# 	array
+# end
+
+# def is_divisible_by_three?(array)
+# 	is_divisible_by?(array, 3)
+# end
+
+# def is_divisible_by_five?(array)
+# 	is_divisible_by?(array, 5)
+# end
+
+# def is_divisible_by_fifteen?(array)
+# 	is_divisible_by?(array, 15)
+# end
+
+# def is_divisible_by?(array, divisor)
+# 	array % divisor == 0
 end
-
-
 
 # 4. Refactored Solution
 
